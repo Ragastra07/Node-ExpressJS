77a52631100cc3357c2e99f964f57f3318d1d6b7
Ragastra07 <haryo.accn.7@gmail.com>
2024-09-29 21:03:57 +0700

    request

--- test/request.test.js
+++ test/request.test.js
@@ -3,6 +3,10 @@ import request from "supertest";
 
 const app = express();
 
+app.get('/', (req, res) => {
+    res.send("Hello World");
+});
+
 test("Test ExpressJS", async () => {
     const response = await request(app).get("/");
     expect(response.text).toBe("Hello World");
